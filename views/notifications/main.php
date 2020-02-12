35bc0db72cef63d91ee915ecb07a33092a7ec8fa
Danilo Alves <daniloalves.ti@gmail.com>
2020-02-12 20:37:36 -0300

    Tratando novas mensagens de erro

--- views/notifications/main.php
+++ views/notifications/main.php
@@ -6,24 +6,40 @@ use ActiveCampaignLists\Helpers\{ Utils, View };
 
 get_header();
 
+if ( ! is_user_logged_in() ) {
+	printf( '<h3>%s</h3>', __( 'É preciso estar logado para visualizar o conteúdo dessa página.', 'wp-active-campaign-lists' ) );
+	get_footer();
+	return;
+}
+
+if ( $missing_credentials ) {
+	printf( '<h3>%s</h3>', __( 'Não foi possível conectar a API. Por favor, verifique as credenciais na administração.', 'wp-active-campaign-lists' ) );
+	get_footer();
+	return;
+}
+
 if ( empty( $lists ) ) {
 	printf( '<h3>%s</h3>', __( 'Nenhuma lista foi cadastrada.', 'wp-active-campaign-lists' ) );
 }
 
 if ( empty( $contact ) ) {
-	printf( '<h3>%s</h3>', __( 'Contato não encontrado.', 'wp-active-campaign-lists' ) );
+	printf( '<h3>%s</h3>', __( 'Usuário não encontrado.', 'wp-active-campaign-lists' ) );
 }
 
 if ( $lists && $contact ) :
 
 ?>
-<section data-contact="<?php echo $contact->id ?>" data-component="lists" class="wp-active-campaign-lists is-loading">
+<section data-contact="<?php echo $contact->id ?>" data-component="lists" class="wp-active-campaign-lists">
+
 	<?php View::render( 'notifications.loader' ); ?>
+
 	<header>
 		<div class="col"><?php esc_html_e( 'Suas Assinaturas', 'wp-active-campaign-lists' ); ?></div>
 		<div class="col"><?php esc_html_e( 'Notificações', 'wp-active-campaign-lists' ); ?></div>
 	</header>
+
 	<?php foreach ( $lists as $list ) : ?>
+
 	<div class="row">
 		<?php $list_id = intval( $list['id'] ); ?>
 		<div class="col"><?php echo esc_html( $list['name'] ); ?></div>
@@ -39,8 +55,12 @@ if ( $lists && $contact ) :
 			<label for="item-<?php echo $list_id; ?>" class="toggle"><span></span></label>
 		</div>
 	</div>
-	<?php endforeach; ?>
-	<?php wp_nonce_field( 'wacl_update_list' ); ?>
+
+	<?php
+	endforeach;
+	wp_nonce_field( 'wacl_update_list' );
+	?>
+
 </section>
 <?php
 
